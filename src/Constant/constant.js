d8b1f12249867d035cb3e64e8826d8d71b079a86
Umesh-roka <umeshmagar764@gmail.com>
2024-08-06 07:42:08 +0545

    change

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,5 +1,5 @@
-const baseUrl = 'https://clone-codepen-back.onrender.com/api';
+const baseUrl = 'http://192.168.56.1:5000/api';
 export const userUrl = `${baseUrl}/users`;
 export const codeUrl = `${baseUrl}/codes`;
 
-// http://192.168.56.1:5000/api
+// /api
